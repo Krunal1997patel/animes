7febcd97784601f3ae396cfe0ad7c36254e12953
Krunal Patel <Pkrunal616@gmail.com>
2020-07-06 23:46:09 -0400

    adding nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import './App.css';
+import Dashboard from './comoned/Dashboard.js';
+import Navbar from './comoned/Navbar.js';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			<Dashboard />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/comoned/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = (props) => {
+	return (
+		<div>
+			<h2>Anime Show</h2>
+		</div>
+	);
+};
+
+export default Navbar;
