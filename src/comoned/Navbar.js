6660eb40ef1a956a7f50710c94798f829b23c78e
Krunal Patel <Pkrunal616@gmail.com>
2020-07-13 21:48:08 -0400

    darkmode

--- src/comoned/Navbar.js
+++ src/comoned/Navbar.js
@@ -4,8 +4,14 @@ const Navbar = (props) => {
 	return (
 		<div>
 			<h2>Anime Show</h2>
+			<div>
+				<a>Search Anime</a>
+				<a>Live Anime</a>
+			</div>
 		</div>
 	);
 };
+
+//darkmode
 
 export default Navbar;
