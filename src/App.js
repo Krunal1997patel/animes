3ff11ec0d6e17acc9b8b7344f291f8b131324a6f
Krunal Patel <Pkrunal616@gmail.com>
2020-09-01 22:13:57 -0400

    addind footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Dashboard from './comoned/Dashboard.js';
 import Navbar from './comoned/Navbar.js';
+import Footer from './comoned/Footer.js';
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
 			<Route exact path='/' component={Dashboard} />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/comoned/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = (props) => {
+	return (
+		<div>
+			<p>copyright | made by krunal</p>
+		</div>
+	);
+};
